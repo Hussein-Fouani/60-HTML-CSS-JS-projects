4dc65026b6c9d45036ed4e0536031be76a490822
Hussein <41930483@students.liu.edu.lb>
2024-01-27 21:55:18 +0200

    Refactor dice rolling functionality and add roll history

--- Project4-Dice-Roller/main.js
+++ Project4-Dice-Roller/main.js
@@ -1,7 +1,28 @@
-const btnclick = document.getElementsByClassName("roll-button");
+const btnclick = document.getElementById("roll-button");
 const diceimg = document.getElementById("dice");
-function rollDice(){
-    
+const rollhistory = document.getElementById("roll-history");
+let historylist = [];
+
+function rollDice() {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  const diceface = getDiceFace(roll);
+  historylist.add(diceface);
+  updaterollhistory();
+}
+function updaterollhistory(){
+    rollhistory.innerHTML ="";
+    for(let i =0;i<historylist.length;i++){
+        const listitem = document.createElement("li");
+        listitem.innerHTML=`Roll ${i+1}: <span>${getDiceFace(historylist[i])}</span>`;
+    }
+}
+function rollDices() {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  return roll;
+}
+
+function getDiceFace(element) {
+  const rols = "&#985" + rollDices();
 }
 btnclick.addEventListener("click", () => {
   diceimg.classList.add("roll-animation");
