5e6c7aa73c877e78d6fd42e05901bf6f650c962c
Hussein <41930483@students.liu.edu.lb>
2024-01-27 22:49:04 +0200

    Fix roll history display in main.js

--- Project4-Dice-Roller/main.js
+++ Project4-Dice-Roller/main.js
@@ -6,27 +6,32 @@ let historylist = [];
 function rollDice() {
   const roll = Math.floor(Math.random() * 6) + 1;
   const diceface = getDiceFace(roll);
-  historylist.add(diceface);
+  historylist.push(diceface);
   updaterollhistory();
 }
-function updaterollhistory(){
-    rollhistory.innerHTML ="";
-    for(let i =0;i<historylist.length;i++){
-        const listitem = document.createElement("li");
-        listitem.innerHTML=`Roll ${i+1}: <span>${getDiceFace(historylist[i])}</span>`;
-    }
+
+function updaterollhistory() {
+  rollhistory.innerHTML = "";
+  for (let i = 0; i < historylist.length; i++) {
+    const listitem = document.createElement("li");
+    listitem.innerHTML = `Roll ${i + 1}: <span>${historylist[i]}</span>`;
+    rollhistory.appendChild(listitem);
+  }
 }
+
 function rollDices() {
   const roll = Math.floor(Math.random() * 6) + 1;
   return roll;
 }
 
 function getDiceFace(element) {
-  const rols = "&#985" + rollDices();
+  return String.fromCodePoint(0x267f + element);
 }
+
 btnclick.addEventListener("click", () => {
   diceimg.classList.add("roll-animation");
   setTimeout(() => {
     diceimg.classList.remove("roll-animation");
+    rollDice();
   }, 1000);
 });
